10036007b1bc78bab8fef9a825daa012144e58b9
GraziaRossi <g.rossi26@studenti.unisa.it>
2020-04-21 14:58:42 +0200

    aggiunta di una stampa in Stampa1

--- src/pkg1/Stampa1.java
+++ src/pkg1/Stampa1.java
@@ -17,6 +17,7 @@ public class Stampa1 {
 		// TODO Auto-generated method stub
 		
 		String s="*****______ Questa è la Stampa1: "+this.s;
+		System.out.println("Modifica da GraziaUnisa");
 		
 		return s;
 	}
